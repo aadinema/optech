c86dd05e4dd9ddb8f270ae2a53a58f88115cfff9
Aaditya Nema <aadinema06@example.com>
2026-02-16 13:24:07 +0530

    fix import error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Navbar from "./components/navbar/Navbar.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import { BrowserRouter } from "react-router-dom";
 
